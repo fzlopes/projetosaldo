d5a9a99d627924889300f73a38a8740bbea04d71
Fabio Lopes <fzlopes1@gmail.com>
2018-02-03 16:18:46 -0200

    Upload da imagem do perfil do usuário e exibição da imagem.

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -14,6 +14,8 @@ class UserController extends Controller
 
    public function profileUpdate(Request $request)
    {
+         $user = auth()->user();
+
    		$data = $request->all();
 
    		if ($data['password'] != null)
@@ -21,7 +23,29 @@ class UserController extends Controller
          else
             unset($data['password']);
 
-   		$update = auth()->user()->update($data);
+   		$data['image'] = $user->image;
+         if ($request->hasFile('image') && $request->file('image')->isValid()) 
+         {
+            if ($user->image)
+               $name = $user->image;
+            else
+               $name = $user->id.kebab_case($user->name);
+
+            $extension = $request->image->extension();
+            $nameFile = "{$name}.{$extension}";
+
+            $data['image'] = $nameFile;
+
+            $upload = $request->image->storeAs('users', $nameFile);
+
+            if (!$upload)
+               return redirect()
+                           ->back()
+                           ->with('error', 'Falha ao fazer o upload da imagem');
+         }
+
+
+         $update = $user->update($data);
 
    		if ($update)
    			return redirect()
--- resources/views/site/profile/profile.blade.php
+++ resources/views/site/profile/profile.blade.php
@@ -8,7 +8,7 @@
 
 @include('site.includes.alerts')
 
-<form action="{{ route('profile.update') }}" method="POST">
+<form action="{{ route('profile.update') }}" method="POST" enctype="multipart/form-data">
     {!! csrf_field() !!}
 	
 	<div class="form-group">
@@ -24,6 +24,10 @@
 		<input type="password" name="password" placeholder="Senha" class="form-control">
 	</div>
 	<div class="form-group">
+		@if (auth()->user()->image != null)
+			<img src="{{ url('storage/users/' .auth()->user()->image) }}" alt="{{ auth()->user()->name }}" style="max-width: 50px;">
+		@endif
+
 		<label for="image">Imagem</label>
 		<input type="file" name="image" class="form-control">
 	</div>
